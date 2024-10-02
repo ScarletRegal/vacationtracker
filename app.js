727a60805458721bf17a00a1ea678699c99b8fa0
= <brendanmiron845@gmail.com>
2024-10-02 09:44:48 -0400

    icons

--- app.js
+++ app.js
@@ -46,17 +46,6 @@ function checkDatesInvalid(startDate, endDate) {
 // add the storage key as an app-wide constant
 const STORAGE_KEY = "vacation_tracker";
 
-function getAllStoredVacations() {
-    // get the string of vacations from localStorage
-    const data = window.localStorage.getItem(STORAGE_KEY);
-
-    // if no vacations are stored, defualt to an empty array
-    // otherwise, return the  stored data (JSON string) as parsed JSON
-    const vacations = data ? JSON.parse(data) : [];
-
-    return vacations;
-};
-
 function storeNewVacation(startDate, endDate) {
     // get data from the storage
     const vacations = getAllStoredVacations(); // returns an array of Strings
@@ -72,6 +61,17 @@ function storeNewVacation(startDate, endDate) {
     // store the new array back in storage
 
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(vacations));
+};
+
+function getAllStoredVacations() {
+    // get the string of vacations from localStorage
+    const data = window.localStorage.getItem(STORAGE_KEY);
+
+    // if no vacations are stored, defualt to an empty array
+    // otherwise, return the  stored data (JSON string) as parsed JSON
+    const vacations = data ? JSON.parse(data) : [];
+
+    return vacations;
 };
 
 function renderPastVacations() {
