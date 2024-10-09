214c0779bd315e6a905b125ee3cae1ddac3b2c36
= <brendanmiron845@gmail.com>
2024-10-09 09:37:49 -0400

    oops

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function formatDate(dateString) {
 renderPastVacations();
 
 // register the service worker
-if ("serviceWork" in navigator) {
+if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js")
     .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
