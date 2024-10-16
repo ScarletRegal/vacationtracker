33ac8a4295fe7dd2fb9dd8b1553cd9e42f836675
= <brendanmiron845@gmail.com>
2024-10-16 09:21:54 -0400

    sw change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v3";
+const VERSION = "v4";
 
 // offline resource list
 const APP_STATIC_RESOURCES = [
@@ -139,9 +139,43 @@ self.addEventListener("sync", function(event) {
 });
 
 function sendDataToServer() {
-    
+    return getAllPendingData()
+        .then(function (dataList) {
+            return Promise.all(
+                dataList.map(function (item) {
+                    // simulate sending the data to the server
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            if (Math.random() > 0.1) { // 90% success rate
+                                console.log("Data sent successfully:", item.data);
+                                resolve(item.id);
+                            } else {
+                                console.log("Failed to send data:", item.data);
+                                reject(new Error("Failed to send data"));
+                            }
+                        }, 1000);
+                    }).then(function () {
+                        // if successful, remove the item from the database
+                        return removeDataFromIndexedDB(item.id);
+                    });
+                })
+            );
+        });
+        
 }
 
 function getAllPendingData() {
-    return new Promise()
+    return new Promise((resolve, reject) => {
+        // transaction to read data from the database
+        const transaction = db.transaction(["pendingData"], "readonly");
+        const objectStore = transaction.objectStore("pendingData");
+        const request = objectStore.getAll();
+
+        request.onsuccess = function (event) {
+            resolve(event.target.result);
+        };
+        request.onerror = function (event) {
+            reject("Error fetching data: " + event.target.error);
+        };
+    })
 }
